b91c1334e5cd7796220af7110bd3cb21d7c27b0b
Yuriy Astapov <yuriyast04@gmail.com>
2026-03-16 18:42:19 -0500

    search page for chemicals and pesticides in the database

--- LifestyleExposureGeneralHealth/lib/supabase.ts
+++ LifestyleExposureGeneralHealth/lib/supabase.ts
@@ -1,13 +1,13 @@
 import 'react-native-url-polyfill/auto'
 import { createClient } from '@supabase/supabase-js'
-import 'expo-sqlite/localStorage/install';
+import { storage } from './storage'
 
 const supabaseUrl = process.env.EXPO_PUBLIC_SUPABASE_URL
 const supabaseAnonKey = process.env.EXPO_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
-    storage: localStorage,
+    storage,
     autoRefreshToken: true,
     persistSession: true,
     detectSessionInUrl: false,
